b941ba9035fe8e7940f718c26cadd341fc976a36
alexandre DT <alexandredt79@gmail.com>
2017-08-18 13:13:54 -0400

    also put the has_many => through on taxons - > kits

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,5 +1,7 @@
 Spree::Taxon.class_eval do
   has_many :lookbooks,foreign_key: "spree_taxon_id"
-  has_many :kits,foreign_key: "spree_taxons_id", class_name:"Spree::Kit"
+  has_many :spree_kits_taxons,  class_name: "Spree::KitTaxon"
+
+  has_many :kits,:through => 'spree_kits_taxons', class_name: "Spree::Kit"
 
 end
